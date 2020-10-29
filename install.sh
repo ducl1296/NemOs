3de590dbfce2dce2bcff218cf02bd259590be92c
ducl1296 <ducl1296@gmail.com>
2020-10-29 14:52:50 -0600

    update

---
+++ install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+apt update && apt upgrade
+apt install xorg i3 slim terminator mousepad lxappearance nemo sudo git unzip feh rofi wget compton xpfd ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp
