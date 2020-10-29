0e6c2ad4d874a18137e2eb6a4b4d820598c9cd2b
ducl1296 <ducl1296@gmail.com>
2020-10-29 15:46:30 -0600

    Live install

--- install.sh
+++ install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-apt update && apt upgrade
-apt install xorg i3 slim terminator mousepad lxappearance nemo sudo git unzip feh rofi wget compton xpfd ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp
+sudo apt update && sudo apt upgrade
+sudo apt install xorg i3 slim terminator mousepad lxappearance nemo git unzip feh rofi wget compton xpfd ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp
+sudo pip3 install youtube-dl
+echo "deb http://ftp.debian.org/debian buster-backports main" | sudo -A tee /etc/apt/sources.list.d/backports.list
+sudo apt -t buster-backports install polybar -y
+git clone https://github.com/ducl1296/NemOs.git
