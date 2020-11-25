08f2fd6ef867b06b097799a06756047757733dbf
ducl1296 <ducl1296@gmail.com>
2020-11-25 15:18:14 -0600

    Update Configure.sh

--- Configure.sh
+++ Configure.sh
@@ -1,8 +1,8 @@
 #Ejecute on non-root
 mkdir /home/"$USER"/.config
 mkdir /home/"$USER"/.local
-cp config/* /home/"$USER"/.config
-cp local/* /home/"$USER"/.local
+cp -r config/* /home/"$USER"/.config
+cp -r local/* /home/"$USER"/.local
 #Allow Bitmaps
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
 sudo rm /etc/mpd.conf
