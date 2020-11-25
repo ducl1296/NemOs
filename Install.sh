725a11643fe46f74336d25983aa2c46dcf293e78
ducl1296 <ducl1296@gmail.com>
2020-11-25 14:54:41 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -3,7 +3,10 @@
 apt install xorg i3 rofi nemo terminator feh compton lxappearance mousepad midori -y
 echo "nemo  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 echo "deb http://ftp.debian.org/debian buster-backports main" | tee /etc/apt/sources.list.d/backports.list
+echo "deb http://ppa.launchpad.net/nemh/systemback/ubuntu xenial main" >> /etc/apt/sources.list
+echo "deb-src http://ppa.launchpad.net/nemh/systemback/ubuntu xenial main " >> /etc/apt/sources.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 73C62A1B
 apt update && apt upgrade
 apt install polybar -y
-apt install ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp lightdm -y
+apt install ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp lightdm policykit-1-gnome -y
 apt purge xterm -y
