e3e2c4b1dcb3e6c7bd0978d0c83c675ccae57241
ducl1296 <ducl1296@gmail.com>
2020-11-25 11:52:02 -0600

    Add files via upload

---
+++ Install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+######Install this from sudo
+apt install xorg i3 rofi nemo terminator feh compton lxappearance mousepad midori -y
+echo "nemo  ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "deb http://ftp.debian.org/debian buster-backports main" | tee /etc/apt/sources.list.d/backports.list
+apt update && apt upgrade
+apt install polybar -y
+apt install ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp -y
+apt purge xterm -y
