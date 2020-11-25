1dc64fad328a1eb8ceaf34b3dd48847124e0e0fc
ducl1296 <ducl1296@gmail.com>
2020-11-25 13:39:04 -0600

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -5,5 +5,5 @@ echo "nemo  ALL=(ALL:ALL) ALL" >> /etc/sudoers
 echo "deb http://ftp.debian.org/debian buster-backports main" | tee /etc/apt/sources.list.d/backports.list
 apt update && apt upgrade
 apt install polybar -y
-apt install ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp -y
+apt install ffmpeg pulseaudio pavucontrol mpd mpc ncmpcpp lightdm -y
 apt purge xterm -y
